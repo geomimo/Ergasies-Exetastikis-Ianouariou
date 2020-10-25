abdfe5ec325eaaaa42abb4707798291e4863ba9a
geomimo <35368032+geomimo@users.noreply.github.com>
2020-10-25 13:58:45 +0200

    Update and rename Askisi_8.py to sum_zero.py

--- sum_zero.py
+++ sum_zero.py
@@ -1,19 +1,23 @@
 import random
+'''
+In this project, a list of random 30 numbers in range [-29,29]/0 is created. The program finds any triplets of numbers that their sum is 0.
+'''
 
-def Check(i,j,k,tr): # checkarei an o syndiasmos yparxei me alli diataxi mesa stin lista oste na apofeyxthoun epanalipseis
+# Checking if any combination of number i, j, k exist in the list.
+def Check(i,j,k,tr): 
     if [i,j,k] in tr or [i,k,j] in tr or [k,i,j] in tr or [k,j,i] in tr or [j,i,k] in tr or [j,k,i] in tr:
         return False
     else:
         return True
 
 while True:
-    lst = random.sample(range(-30,30),30) #dimiourgia pinaka me mono thetikous kai arnitikous opos ziteitai
+    lst = random.sample(range(-30,30),30)
     if not (0 in lst):
         break
 
-f=False #flag gia tin eyresi toulaxiston mias triadas
-triades=[] #sygkentrosi triadon
-for i in lst: #kathe syndiasmos
+f=False 
+triades=[] 
+for i in lst:
     for j in lst:
         for k in lst:
             if i+j+k==0 and Check(i,j,k,triades):
@@ -23,6 +27,6 @@ for i in lst: #kathe syndiasmos
 if f==False:
     print "Den vrethike triada"
 else:
-    for tr in triades: #taxinomisi gia omoiomorfia kai ektiposi
+    for tr in triades: 
         tr.sort()
         print tr
